0c28a8ae85024cfe0bb430ce8ce5929c5e2d03a7
mindera-dev <122076493+mindera-dev@users.noreply.github.com>
2023-03-15 16:16:55 +0900

    Update sftp_odm.py

--- workflow/scripts/sftp_odm.py
+++ workflow/scripts/sftp_odm.py
@@ -8,11 +8,12 @@ import s3_exec
 def main():    
     #Run export(sftp) odm(match-1)
     print('sftp_odm start!!')
+    '''
     res = k8s_exec.exec_commands('sftpodm', '779792627677.dkr.ecr.us-west-2.amazonaws.com/lambda-py:v2.0.0', 'python3 mlops-de-get-odm.py')
     print(res)
 
     #make result file
-    '''
+    
     f = open('/dags/mlops-pipeline-result/sftp_odm.txt','w')
     f.write(str(res))
     f.close()    
