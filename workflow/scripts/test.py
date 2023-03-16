9bd9fe47e327c2fd70f86967cbfb5a055761e916
mindera-dev <122076493+mindera-dev@users.noreply.github.com>
2023-03-16 13:20:09 +0900

    Update test.py

--- workflow/scripts/test.py
+++ workflow/scripts/test.py
@@ -9,4 +9,32 @@ s3 = boto3.client(
 )
 print('s3: client')
 
+paginator = client.get_paginator('list_objects_v2')
+response_iterator = paginator.paginate(
+    Bucket=mlops-lims-dump-prod, 
+    Prefix="output_files/"
+)
 
+compare_file_string = "-Reads-Counts-and-logCPM.csv"
+file_list=[]
+try:
+    for page in response_iterator:
+        for content in page['Contents']:
+            # print(content)
+            key = content['Key']
+            # print("key:", key)
+            if compare_file_string in key:    
+                split_file = key.split("/")
+                # print("split_file:",split_file)
+                if len(split_file) == 4:
+                    file_list.append({'bucket': os_env_access_bucket_name, 'batch': split_file[1], 'sample': split_file[2]})
+                    #return file_list
+
+                else:
+                    print(key)
+
+    return file_list
+
+except Exception as e:
+    print(e)
+    raise e
