c988e0bcbed7a9334ed6c4b9de04980523526fef
mindera-dev <122076493+mindera-dev@users.noreply.github.com>
2023-03-16 19:44:27 +0900

    Update sftp_odm.py

--- workflow/scripts/sftp_odm.py
+++ workflow/scripts/sftp_odm.py
@@ -18,12 +18,22 @@ def main():
     #Run export(sftp) odm(match-1)
     print('sftp_odm start!!')
     
-    res = exec_commands('sftpodm', '779792627677.dkr.ecr.us-west-2.amazonaws.com/lambda-py:v2.0.0', 'python3 mlops-de-get-odm.py')
+    config.load_kube_config()
+    try:
+        c = Configuration().get_default_copy()
+    except AttributeError:
+        c = Configuration()
+        c.assert_hostname = False
+    Configuration.set_default(c)
+    core_v1 = core_v1_api.CoreV1Api()
+    
+    #res = exec_commands('sftpodm', '779792627677.dkr.ecr.us-west-2.amazonaws.com/lambda-py:v2.0.0', 'python3 mlops-de-get-odm.py')
+    res = exec_commands('sftpodm', '779792627677.dkr.ecr.us-west-2.amazonaws.com/lambda-py:v2.0.0', 'python3 mlops-de-get-odm.py', core_v1)
     print(res)
 
     #make result file
     
-    f = open('/dags/mlops-pipeline-result/sftp_odm.txt','w')
+    f = open('dags/mlops-pipeline-result/sftp_odm.txt','w')
     f.write(str(res))
     f.close()    
 
