6f724ff0be5945107079a66fae6fe624bd0db59e
mindera-dev <122076493+mindera-dev@users.noreply.github.com>
2023-03-17 17:14:04 +0900

    Update load_lims.py

--- workflow/scripts/load_lims.py
+++ workflow/scripts/load_lims.py
@@ -1,5 +1,10 @@
-import k8s_exec
-import s3_exec
+import time
+import os
+from kubernetes import config
+from kubernetes.client import Configuration
+from kubernetes.client.api import core_v1_api
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
 # 
 # - Comment : Run load lims
 #             , make result file
@@ -8,11 +13,29 @@ import s3_exec
 
 def main():
     #Run load lims
-    res = k8s_exec.exec_commands('loadlims', '779792627677.dkr.ecr.us-west-2.amazonaws.com/minderadatatransfer:V1.0.1', '/java/datatransfer.sh lims 2022-12-21')
+    print('export_lims start!!')    
+    
+    config.load_kube_config()
+    try:
+        c = Configuration().get_default_copy()
+    except AttributeError:
+        c = Configuration()
+        c.assert_hostname = False
+    Configuration.set_default(c)
+    core_v1 = core_v1_api.CoreV1Api()
+    
+    res = k8s_exec.exec_commands('loadlims', '779792627677.dkr.ecr.us-west-2.amazonaws.com/minderadatatransfer:V1.0.2', '/java/datatransfer.sh lims 2023-03-16')
     print(res)
 
     #make result file
-    f = open('/dags/mlops-pipeline-result/load_lims.txt','w')
+    directory = "/home/ubuntu/mlops-de-pipeline/2023-03-17"
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print("Error: Failed to create the directory.")
+    
+    f = open(directory + '/load_lims.txt','w')
     f.write(str(res))    
     f.close()
 
